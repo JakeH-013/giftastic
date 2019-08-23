45302345c4974ba4a37e3682ce62187c4931dab9
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-08-22 23:23:19 -0400

    fixed syntax linking my js file to the html file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,18 @@
-var LotR = ["Aragorn", "Frodo Baggins", "Bilbo Baggins", 
-            "Gimli", "Theoden", "Gandalf", "Sauron", "Rohan", "Eomer"];
+var lotr = ["Aragorn", "Frodo Baggins", "Bilbo Baggins",
+    "Gimli", "Theoden", "Gandalf", "Sauron", "Rohan", "Eomer"];
 
-function createButtons() {
-    
+//function to generate buttons
+function makeBtns() {
+    //deletes buttons before buttons are added
+    $("#btn-section").empty();
+    //generates button for each item in array
+    for (var i = 0; i < lotr.length; i++) {
+        var b = $("<button>")
+            .addClass("lotr-btn")
+            .attr("data-name", lotr[i])
+            .text(lotr[i]);
+        //adds buttons to btn-sectin div
+        $("#btn-section").append(b);
+    }
 }
+makeBtns();
