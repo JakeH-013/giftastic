dcbeb87b82505406cbc3b521798977009aad9344
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-08-24 17:20:46 -0400

    added queryUrl variable and tested that it works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,16 @@
 var lotr = ["Aragorn", "Frodo Baggins", "Bilbo Baggins",
     "Boromir", "Theoden", "Gandalf", "Sauron", "Rohan", "Eomer"];
 
+function produceGifs() {
+    var lotrName = $(this).attr("data-name");
+    console.log(lotrName);
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+    lotrName + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    console.log(queryUrl);
+}
+
+
+
 //function to generate buttons
 function makeBtns() {
     //deletes buttons before buttons are added
@@ -8,7 +18,7 @@ function makeBtns() {
     //generates button for each item in array
     for (var i = 0; i < lotr.length; i++) {
         var b = $("<button>")
-            .addClass("lotr-btn")
+            .addClass("lotr")
             .attr("data-name", lotr[i])
             .text(lotr[i]);
         //adds buttons to btn-sectin div
@@ -26,5 +36,7 @@ $("#name-add").on("click", function(event) {
 
     makeBtns();
 })
+
+$(document).on("click", ".lotr", produceGifs);
 
 makeBtns();
