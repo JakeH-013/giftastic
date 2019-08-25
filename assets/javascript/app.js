f9c1b55768804a29738a734c1e6aa4e302332868
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-08-24 21:52:40 -0400

    moved gif click event so that gifs would animate; working on making all the gifs animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,44 +6,55 @@ function produceGifs() {
     var lotrName = $(this).attr("data-name");
     //forms giffy url for the name/word clicked
     var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-    lotrName + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+        lotrName + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
     //performs ajax GET request
     $.ajax({
         url: queryUrl,
         method: "GET"
-      })
-      //triggers function after the data comes from the API
-      .then(function(response) {
-          console.log(response)
-        //stores an array of the results in a variable
-        var results = response.data;
-        //for loop for each result item
-        for (var i = 0; i < results.length; i++) {
-            //div for the gif
-            var gifDiv = $("<div>");
-            //varaible containing the gif rating
-            var rating = results[i].rating;
-            //creates paragraph for the rating
-            var r = $("<p>").text("Rating: " + rating);
-            //image tag with a source pulled from the result
-            var lotrImg = $("<img>")
-                .addClass("gif")
-                .attr("src", results[i].images.fixed_height_still.url);
-            //appends rating and gif to the gifDiv div
-            gifDiv.append(r);
-            gifDiv.append(lotrImg);
-            //prepends gifDiv to html
-            $("#gif-section").prepend(gifDiv);
-        }
-      })
+    })
+        //triggers function after the data comes from the API
+        .then(function (response) {
+            //stores an array of the results in a variable
+            var results = response.data;
+            console.log(results)
+            //for loop for each result item
+            for (var i = 0; i < results.length; i++) {
+                //div for the gif
+                var gifDiv = $("<div>");
+                //varaible containing the gif rating
+                var rating = results[i].rating;
+                //creates paragraph for the rating
+                var r = $("<p>").text("Rating: " + rating);
+                //image tag with a source pulled from the result
+                var lotrImg = $("<img>")
+                    .addClass("gif")
+                    .attr("src", results[i].images.original_still.url)
+                    .attr("data-state", "still")
+                    .attr("data-still", results[i].images.original_still.url)
+                    .attr("data-animate", results[i].images.original.url);
+                console.log(lotrImg)
+                //appends rating and gif to the gifDiv div
+                gifDiv.append(r);
+                gifDiv.append(lotrImg);
+                //prepends gifDiv to html
+                $("#gif-section").prepend(gifDiv);
+
+                $(".gif").on("click", function () {
+                    var state = $(this).attr("data-state");
+                
+                    if (state === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr("src", $(this).attr("data-still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+            }
+        })
 
 }
-
-// $(".gif").on("click", function() {
-
-
-// })
 
 
 //function to generate buttons
@@ -61,7 +72,7 @@ function makeBtns() {
     }
 }
 
-$("#name-add").on("click", function(event) {
+$("#name-add").on("click", function (event) {
     //prevents default form action
     event.preventDefault();
     //grabs input box text
